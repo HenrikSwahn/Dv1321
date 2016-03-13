7b7d39ec4b6931960044bf4c4c4d184b439499c1
Henrik Swahn <hesw91@gmail.com>
2016-03-13 14:23:12 +0100

    Written many parts of the introduction, also a c++ version of matmul <- will be replaced with a C version

--- Experiment/MatrixMul/Pthreads/matmulCalc.cpp
+++ Experiment/MatrixMul/Pthreads/matmulCalc.cpp
@@ -5,8 +5,10 @@
 #include <pthread.h>
 #include "matrix.h"
 
-#define SIZE 1000
-#define T_MAX 4;
+#define SIZE 2
+#define T_MAX 2
+
+pthread_t tid[T_MAX];
 
 using namespace std;
 
@@ -16,17 +18,25 @@ typedef struct {
 	Matrix* matrix_c;
 } MatrixContainer;
 
+typedef struct {
+	MatrixContainer* container;
+	int X_begin;
+	int Y_begin;
+	int X_end;
+	int Y_end;
+}ThreadParam;
+
 void init(Matrix*);
-void matmul(Matrix*, Matrix*, Matrix *);
+void* matmul(void *);
 
 int main() {
 	srand(time(NULL));
-
+		
 	MatrixContainer* container = new MatrixContainer;
 	container->matrix_a = new Matrix(SIZE);
 	container->matrix_b = new Matrix(SIZE);
 	container->matrix_c = new Matrix(SIZE);
-	
+
 	clock_t initTime = clock(), initDiff;
 	init(container->matrix_a);
 	init(container->matrix_b);
@@ -36,18 +46,34 @@ int main() {
 	cout << "Took: " << initTimeMsec << " msec to init matrix_a and matri_b" << endl;
 
 	clock_t matTime = clock(), matDiff;
-	matmul(
-		container->matrix_a,
-		container->matrix_b,
-		container->matrix_c
-  	);
+
+	int x = 0;
+	int y = 0;
+
+	for(int i = 0; i < T_MAX; ++i) {
+		
+		ThreadParam* param = new ThreadParam;
+		param->container = container;
+		param->X_begin = x;
+		param->X_end = x + (SIZE/T_MAX);
+
+		pthread_create(&tid[i], NULL, matmul, (void*)param);
+
+		x += SIZE / T_MAX;
+	}
+
+	for(int i = 0; i < T_MAX; ++i) {
+		pthread_join(tid[i], NULL);
+	}
+	
 	matDiff = clock() - matTime;
 
 	int matTimeMsec = matDiff * 1000 / CLOCKS_PER_SEC;
 	cout << "Took: " << matTimeMsec << " msec to matmul matrix_a and matrix_b" << endl;
-	// cout << matrix_a->toString() << endl;
-	// cout << matrix_b->toString() << endl;
-	// cout << matrix_c->toString() << endl;
+	
+	cout << container->matrix_a->toString() << endl;
+	cout << container->matrix_b->toString() << endl;
+	cout << container->matrix_c->toString() << endl;
 
 	delete(container->matrix_a);
 	delete(container->matrix_b);
@@ -65,18 +91,20 @@ void init(Matrix* matrix) {
 	}
 }
 
-Matrix* matmul(Matrix* a, Matrix* b, Matrix *c) {
-
-	for(int i = 0, sizeX = a->getSize(); i < sizeX; ++i) {
-		for(int j = 0, sizeY = b->getSize(); j < sizeY; j++) {
+void* matmul(void* threadParam) {
+	ThreadParam* param = (ThreadParam*)threadParam;
+	int size = param->container->matrix_a->getSize();
+	
+	for(int i = param->X_begin; i < param->X_end + SIZE/T_MAX; ++i) {
+		for(int j = 0; j < SIZE; j++) {
 			int val = 0;
-			for(int k = 0, sizeK = b->getSize(); k < sizeK; ++k) {
-				val += a->getElement(i,k) * b->getElement(k,j);
-				c->insertElement(i,j,val);
+			for(int k = param->X_begin; k < param->X_end + SIZE/T_MAX; ++k) {
+				val += param->container->matrix_a->getElement(i,k) * param->container->matrix_b->getElement(k,j);
+				param->container->matrix_c->insertElement(i,j,val);
 			}
 		}
 	}
 
-	return c;
+	delete(param);
 }
 
---
+++ Experiment/MatrixMul/Pthreads/matrix.cpp
@@ -0,0 +1,72 @@
+#include <time.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include "matrix.h"
+
+Matrix::Matrix() {
+	size = 10;
+	matrix = new int*[size];
+	setup();
+}
+
+Matrix::Matrix(int size) {
+	this->size = size;
+	matrix = new int*[this->size];
+	setup();
+}
+
+Matrix::~Matrix() {
+
+	cout << "De-constructing matrix...." << endl;
+
+	for(int i = 0; i < size; ++i) {
+		delete(matrix[i]);
+	}
+
+	delete(matrix);
+}
+
+void Matrix::setup() {
+
+	srand(time(NULL));
+
+	for(int i = 0; i < size; ++i) {
+		matrix[i] = new int[size];
+	}
+}
+
+int Matrix::getElement(int X, int Y) {
+
+	if(X >= 0 && X < size) {
+		if(Y >= 0 && Y < size) {
+			return matrix[X][Y];
+		}
+	}
+
+	return -1;
+}
+
+void Matrix::insertElement(int X, int Y, int element) {
+	if(X >= 0 && X < size) {
+		if(Y >= 0 && Y < size) {
+			matrix[X][Y] = element;
+		}
+	}	
+}
+
+int Matrix::getSize() {
+	return size;
+}
+
+string Matrix::toString() {
+	ostringstream output;
+
+	for(int i = 0; i < size; ++i) {
+		for(int j = 0; j < size; ++j) {
+			output << matrix[i][j] << endl;
+		}
+	}
+
+	return output.str();
+}
