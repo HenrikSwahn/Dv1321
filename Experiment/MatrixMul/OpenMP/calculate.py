1423bbe3546b494cc5d625fac9558cb68fefe608
Henrik Swahn <hesw91@gmail.com>
2016-04-14 13:55:53 +0200

    Standard deviation

--- Experiment/MatrixMul/OpenMP/calculate.py
+++ Experiment/MatrixMul/OpenMP/calculate.py
@@ -1,27 +1,9 @@
 import math
+import numpy
 
-def get_average(data): 
-	return (sum(data) / 2)
+def average(data):
+	return sum(data) * 1.0/len(data)
 
-def calc_deviation(data, average):
-	deviations = []
-	for number in data:
-		deviations.append(number - average)
-
-	return deviations
-
-def calc_pow(data):
-	powResults = []
-	for number in data:
-		powResults.append(math.pow(number, 2))
-	return powResults
-
-def standardDeviation(data):
-	average = get_average(data)
-	deviations = calc_deviation(data, average)
-	powDev = calc_pow(deviations)
-	devAverage = get_average(powDev)
-	return math.sqrt(devAverage)
 
 floats = []
 
@@ -29,4 +11,9 @@ with open("result.txt", "r") as f:
 	for line in f:
 		floats.append(float(line))
 
-print(standardDeviation(floats))
+
+avg = average(floats)
+variance = map(lambda x:(x-avg)**2, floats)
+standard_deviation = math.sqrt(average(variance))
+
+print(standard_deviation)
