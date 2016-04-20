95d78c8f53be4621dcf4a8a610709c5a9d41e6fe
Henrik Swahn <hesw91@gmail.com>
2016-04-20 16:22:10 +0200

    Revert quicksort

--- Experiment/Quicksort/OpenMP/quicksort.c
+++ Experiment/Quicksort/OpenMP/quicksort.c
@@ -1,4 +1,4 @@
-/*#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <omp.h>
@@ -86,88 +86,4 @@ int main(int argc, char **argv) {
         logResult(quickStart,quickEnd);
     //}
 	//logOrder(array, MAX_ITEMS);
-}*/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <omp.h>
-
-#define KILO (1024)
-#define MEGA (1024*1024)
-//#define MAX_ITEMS (64*MEGA)
-#define MAX_ITEMS 100
-#define swap(v, a, b) {unsigned tmp; tmp=v[a]; v[a]=v[b]; v[b]=tmp;}
-
-static int *v;
-
-int partition(int * a, int p, int r) {
-    int lt[r-p];
-    int gt[r-p];
-    int i;
-    int j;
-    int key = a[r];
-    int lt_n = 0;
-    int gt_n = 0;
-
-	#pragma omp parallel for
-    for(i = p; i < r; i++) {
-        if(a[i] < a[r]) {
-            lt[lt_n++] = a[i];
-        }
-        else {
-            gt[gt_n++] = a[i];
-        }   
-    }   
-
-    for(i = 0; i < lt_n; i++){
-        a[p + i] = lt[i];
-    }   
-
-    a[p + lt_n] = key;
-
-    for(j = 0; j < gt_n; j++){
-        a[p + lt_n + j + 1] = gt[j];
-    }   
-
-    return p + lt_n;
-}
-
-void quicksort(int * a, int p, int r) {
-    int div;
-
-    if(p < r) { 
-        div = partition(a, p, r); 
-		#pragma omp parallel sections
-        {   
-			#pragma omp section
-            quicksort(a, p, div - 1); 
-			#pragma omp section
-            quicksort(a, div + 1, r); 
-
-        }
-    }
-}
-
-void init() {
-    int i;
-    v = (int *) malloc(MAX_ITEMS*sizeof(int));
-    for (i = 0; i < MAX_ITEMS; i++) {
-        array[i] = rand();
-	}
-}
-
-static void print() {
-    int i;
-
-    for (i = 0; i < MAX_ITEMS; i++) {
-        printf("%d\n", v[i]);
-    }
-    printf("\n");
-}
-
-int main() {
-	init();
-    quicksort(v, 0, MAX_ITEMS-1);
-    print();
-    return 0;
 }
